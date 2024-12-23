4b81256ddf2acd566494f3bda19f58cd04b683bc
wirapradina <146178363+wirapradina@users.noreply.github.com>
2024-12-23 22:24:40 +0700

    Update

--- src/redux/action/reducer/handleProducts.js
+++ src/redux/action/reducer/handleProducts.js
@@ -3,13 +3,13 @@ const initialState = [];
 const handleProducts = (state = initialState, action) => {
     switch (action.type) {
         case 'UPDATE_PRODUCT_QTY':
-            console.log('Reducer received action payload:', action.payload); // Pastikan data yang dikirim sudah benar
-            console.log('Current state:', state); // Verifikasi state yang ada
+            console.log('Reducer received action payload:', action.payload);
+            console.log('Current state:', state);
             return state.map((product) =>
                 product.id === action.payload.productId
                     ? {
                         ...product,
-                        qty: Math.max(product.qty - action.payload.qty, 0), // Mengurangi qty produk yang sudah dibeli
+                        qty: Math.max(product.qty - action.payload.qty, 0), 
                     }
                     : product
             );
